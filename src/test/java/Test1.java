1c00d89428d1d62e559ea65bf617cdcf3330d461
Buddygithub1 <devika36236@gmail.com>
2020-08-03 16:53:49 +0100

    First file

---
+++ src/test/java/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+
+    public static void main (String[] args){
+        System.out.println("My code line 1");
+        System.out.println("My code line 2");
+        System.out.println("My code line 3");
+        System.out.println("--------------");
+
+    }
+}
