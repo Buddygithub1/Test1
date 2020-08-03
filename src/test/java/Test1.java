f9579661023badcd9066edb8a2e88e9721f34b85
Krishna Chaitanya <krishna.chaitanya@directlinegroup.co.uk>
2020-08-03 17:33:55 +0100

    test changes

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,8 +1,8 @@
 public class Test1 {
 
     public static void main (String[] args){
-        System.out.println("My code line 1");
-        System.out.println("My code line 2");
+        System.out.println("My code line 10");
+        System.out.println("My code line 20");
         System.out.println("My code line 3");
         System.out.println("--------------");
         System.out.println("My code line 4");
--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,6 +3,7 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("My code line 4");
         System.out.println("My code line 5");
-
+        System.out.println("My code line 400");
+        System.out.println("My code line 500");
     }
 }
