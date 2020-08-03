3feb4e08c51f6438570d1617c05e14873c570029
Buddygithub1 <devika36236@gmail.com>
2020-08-03 17:34:32 +0100

    Devika new lines

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,9 +2,9 @@ public class Test1 {
 
     public static void main (String[] args){
         System.out.println("My code line 1");
-        System.out.println("My code line 2");
+        System.out.println("My code line 20000");
         System.out.println("My code line 3");
-        System.out.println("--------------");
+        System.out.println("--------------extra-----------");
         System.out.println("My code line 4");
         System.out.println("My code line 5");
         System.out.println("My code line 6");
--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,6 +3,8 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("My code line 4");
         System.out.println("My code line 5");
+        System.out.println("My code line 10");
+        System.out.println("My code line 11");
 
     }
 }
